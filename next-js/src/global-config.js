abfed247c4fa62f84ab44900fd78df4580ba1498
jaffarIOTA <jaffar@iotatechnologies.ai>
2026-01-02 20:51:11 +0300

    fix: update authentication method from jwt to supabase in global config

--- next-js/src/global-config.js
+++ next-js/src/global-config.js
@@ -18,7 +18,7 @@ export const CONFIG = {
    * @method jwt | amplify | firebase | supabase | auth0
    */
   auth: {
-    method: 'jwt',
+    method: 'supabase',
     skip: false,
     redirectPath: paths.dashboard.root,
   },
